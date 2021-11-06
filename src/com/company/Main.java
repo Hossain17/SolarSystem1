a15079ab0eff7e03410c10c408d3d464533dce44
Hossain17 <imam09hossain@gmail.com>
2021-11-06 23:55:24 +0600

    Planet2 has been added successfully

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,13 @@ public class Main {
         mikesun.radius="90000000km";
         mikesun.maritalStatus="in love with asghar";
 
+        //adding planet2 features
+
+        planet2 bastranout=new planet2();
+        bastranout.color="pinnk";
+        bastranout.size="4545564km";
+        bastranout.status="same as MIKE's";
+
 
     }
 }
